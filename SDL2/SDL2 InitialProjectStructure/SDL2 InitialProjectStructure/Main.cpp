3b14de2539fc9f43fc768933536bea477f51e208
Wesley Ferreira <contato.wes.ferr@gmail.com>
2018-08-21 13:58:58 -0300

    SDL project VS template 1.0

--- SDL2/SDL2 InitialProjectStructure/SDL2 InitialProjectStructure/Main.cpp
+++ SDL2/SDL2 InitialProjectStructure/SDL2 InitialProjectStructure/Main.cpp
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 {
 	SDL_Event e;
 	bool running = true;
-	App::getInstance()->getWindows[0].setBorders(false);
 	App::getInstance()->init(); // Init all SDL systems and all windows
 
 	while (running)
